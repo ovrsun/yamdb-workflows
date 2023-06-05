16025244cfc31792d59926a09c83a9f3b2ab456e
Alexey Targanov <siddhett@gmail.com>
2023-06-05 23:22:47 +0200

    Aaaa!11

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['qwerty.ddns.net']
 
 
 # Application definition
